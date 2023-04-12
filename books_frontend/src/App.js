932780fef85ac37c7dda0aee69884350fcaeeb2d
Alexander Parisi <alexanderparisi@Alexanders-MacBook-Air-2.local>
2023-04-12 11:39:54 -0400

    app.js setup

---
+++ books_frontend/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const App = () => {
+  let [books, setBooks] = useState([])
+
+  const getBooks = () => {
+    axios
+      .get('http://')
+      .then(
+        (response) => setPeople(response.data),
+        (err) => console.error(err)
+      )
+      .catch((error) => console.error(error)
+      )
+  }
+
+useEffect(() => {
+ getBooks()
+}, [])
+
+
+  return (
+    <>
+      <h1>Library</h1>
+      <div>
+        {books.map((book) => {
+          return (
+            <div key={book.id}>
+              <h4>Title: {book.book_title}</h4>
+              <h5>Author: {book.author}</h5>
+            </div>
+          )
+        })}
+      </div>
+    </>
+  )
+}
+
+export default App
