d3e5c95a8acea2ca75af71cf8c25f8635075048a
ingrid gonzalez <francisco214.fc@gmail.com>
2023-04-14 16:36:39 -0500

    updated

--- books_frontend/src/App.js
+++ books_frontend/src/App.js
@@ -1,60 +1,64 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import Edit from './components/edit'
-import Add from './components/Add'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Edit from "./components/edit";
+import Add from "./components/Add";
 
 const App = () => {
-  const URL = 'https://books39.p.rapidapi.com/CZFA4F/books/?rapidapi-key='
-  const key = '023f447a90msh6babfc2e25f744cp16d63ejsn393297bc3bc4'
-  const [books, setBooks] = useState([])
+  const URL = "https://books39.p.rapidapi.com/CZFA4F/books/?rapidapi-key=";
+  const key = "023f447a90msh6babfc2e25f744cp16d63ejsn393297bc3bc4";
+  const [books, setBooks] = useState([]);
   // const [searchTerm, setSearchTerm] = useState('')
   const getBooks = async () => {
     try {
-      const response = await axios.get(`${URL}${key}`)
-      setBooks(response.data)
-      console.log(books)
+      const response = await axios.get(`${URL}${key}`);
+      setBooks(response.data);
+      console.log(books);
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
+  const handleDelete = (event) => {
+    axios.delete("http://" + event.target.value).then((response) => {
+      getBooks();
+    });
+  };
 
-const handleDelete = (event) => {
-  axios
-    .delete('http://' + event.target.value)
-    .then((response) => {
-      getBooks()
-    })
-}
+  const handleUpdate = (editBook) => {
+    console.log(editBook);
+    axios.put("http://" + editBook.id, editBook).then((response) => {
+      getBooks();
+    });
+  };
 
-
-const handleUpdate = (editBook) => {
-  console.log(editBook)
-  axios
-    .put('http://' + editBook.id, editBook)
-    .then((response) => {
-      getBooks()
-    })
-}
-      
   useEffect(() => {
-    getBooks()
-  }, [])
-
+    getBooks();
+  }, []);
 
   return (
-    <> 
-    <div>
-    {books.map((books) =>{
-      return(
-        <div>
-          <h1>{books.AUTHOR}</h1>
-        </div>
-      )
-    })}
-    </div>
+    <>
+      {/* <div key={book.id}>
+        <h4>Title: {book.book_title}</h4>
+        <h5>Author: {book.author}</h5>
+        <Edit handleUpdate={handleUpdate} book={book} />
+        <button onClick={handleDelete} value={book.id}>
+          X
+        </button>
+      </div> */}
+      <div>
+        {books.map((books) => {
+          return (
+            <div>
+              <h1>{books.AUTHOR}</h1>
+              <h1>{books.TITLE}</h1>
+              <h1>{books.YEAR}</h1>
+              <h1>{books.IBN}</h1>
+            </div>
+          );
+        })}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
